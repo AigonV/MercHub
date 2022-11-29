bf58793bdec07058aa37d35afcd060eba9ae9551
AigonV <88944725+AigonV@users.noreply.github.com>
2022-11-29 18:31:42 +0100

    Update linoria.lua

--- linoria.lua
+++ linoria.lua
@@ -6,7 +6,7 @@ local RenderStepped = game:GetService('RunService').RenderStepped;
 local LocalPlayer = game:GetService('Players').LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
 
-local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
+--local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
